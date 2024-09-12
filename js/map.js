8cda46d4c04333119a1fc87eb809fcc240240943
Vitue168 <chhoytue2002@gmail.com>
2024-09-12 14:14:10 +0700

    add

--- js/map.js
+++ js/map.js
@@ -81,7 +81,7 @@ function setMapToCurrentLocation() {
 }
 
 // Base CORS proxy URL
-const corsProxy = 'https://corsproxy.io/?'; 
+const corsProxy = 'https://cors-anywhere.herokuapp.com/';
 
 // Fetch data from JSON file via CORS proxy
 fetch(corsProxy + "http://180.178.127.119:8282/map_ptt/data/markers.json")
